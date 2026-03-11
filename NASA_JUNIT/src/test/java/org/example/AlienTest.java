226ab708dbf1e8cbc2fe8793a2b68e97c56204f4
DV-5\ericr <ericrp188@gmail.com>
2026-03-11 12:20:18 +0100

    Prueba con fallo deliverado

--- NASA_JUNIT/src/test/java/org/example/AlienTest.java
+++ NASA_JUNIT/src/test/java/org/example/AlienTest.java
@@ -46,15 +46,39 @@ class AlienTest
     @Test
     void setPlanetId()
     {
+        a1.setPlanetId("M1");
+        a2.setPlanetId("L4");
+        a3.setPlanetId("M1");
+
+        assertAll(
+                () -> assertEquals("M1", a1.getPlanetId()),
+                () -> assertEquals("L4", a2.getPlanetId()),
+                () -> assertEquals("M1", a3.getPlanetId())
+        );
     }
 
     @Test
     void testEquals()
     {
+        assertAll(
+                () -> assertTrue(a1.equals(a3)),
+                () -> assertFalse(a1.equals(a2))
+        );
     }
 
     @Test
     void testToString()
     {
+        assertAll(
+                () -> assertEquals("Zorg MARS01", a1.toString()),
+                () -> assertEquals("Xenomorph LV426", a2.toString()),
+                () -> assertEquals("Zorg MARS01", a3.toString())
+        );
+    }
+
+    @Test
+    void testToStringFallo()
+    {
+        assertEquals("Child of Burning Time", a3.toString());
     }
 }
